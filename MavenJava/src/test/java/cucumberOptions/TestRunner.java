7ff74493b2e19254f2c1dded86420f01f55cbaf3
jobby-tech <70310971+jobby-tech@users.noreply.github.com>
2021-09-02 19:05:37 +0530

    Update TestRunner.java
    Junit to TestNG update

--- MavenJava/src/test/java/cucumberOptions/TestRunner.java
+++ MavenJava/src/test/java/cucumberOptions/TestRunner.java
@@ -1,18 +1,20 @@
-package cucumberOptions;
+package cucumberopts;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(      //Annotation which take the feature file location
-		
-		features ="src/test/java/features",
-		glue = "stepDefinitions"    // parameter to define the stepdefinition files
-		)
-public class TestRunner {
-	
-	
+
+//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/features",
+		glue = "stepDefinition")
+
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
